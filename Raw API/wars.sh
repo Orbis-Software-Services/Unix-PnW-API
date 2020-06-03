9eca0192f63edd0155468ed6ea7de31780141fd7
ApocalypseCalculator <>
2020-06-03 17:26:59 -0400

    Update wars.sh

--- Raw API/wars.sh
+++ Raw API/wars.sh
@@ -1,6 +1,6 @@
 
 #!/bin/sh
-echo "Enter number of wars you want to fetch (min is 1 max is 10,000): "
+echo "Enter number of wars you want to fetch (min is 1 max is 5,000): "
 read numb
 echo "Enter your api key: "
 read key
